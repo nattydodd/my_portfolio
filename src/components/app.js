643a6d666cb2ec61b306f198b315cb69199c7a4e
Natalie Dodd <nataliedodd89@gmail.com>
2017-02-09 22:54:23 +0100

    added react full page and styled top nav

--- src/components/app.js
+++ src/components/app.js
@@ -4,16 +4,18 @@ import WebsiteList from '../containers/website-list';
 import WebsiteDetail from '../containers/website-detail';
 import VideoList from '../containers/video-list';
 import VideoDetail from '../containers/video-detail';
+import FullpageReact from '../containers/full-page-react'
 
 
 export default class App extends Component {
   render() {
     return (
-      <div className="container">
+      <div className="container-fluid page-wrapper">
+        <FullpageReact />
         <section>
-          <div className="row">
-            <WebsiteList />
+          <div className="row website">
             <WebsiteDetail />
+            <WebsiteList />
           </div>
         </section>
         <section>
---
+++ src/containers/full-page-react.js
@@ -0,0 +1,90 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+const {Fullpage, Slide, TopNav, SideNav} = require('fullpage-react');
+
+let topNavOptions = {
+  footer: false, //topNav can double as a footer if true
+  align: 'center', //also supports center and right alignment
+
+  //styles to apply to children
+  activeStyles: {backgroundColor: 'white'},
+  hoverStyles: {backgroundColor: 'yellow'},
+  nonActiveStyles: {backgroundColor: 'gray'}
+};
+
+let sideNavOptions = {
+  right: '2%', //left alignment is default
+  top: '50%', //top is 50% by default
+
+  //styles to apply to children
+  activeStyles: {color: 'white'},
+  hoverStyles: {color: 'yellow'},
+  nonActiveStyles: {color: 'gray'}
+};
+
+class FullpageReact extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {active: 0};
+
+    this.updateActiveState = this.updateActiveState.bind(this);
+  }
+
+  updateActiveState(newActive) {
+    this.setState({'active': newActive});
+  }
+
+  shouldComponentUpdate(nP, nS) {
+    //ensure hoverStyles and activeStyles update
+    return nS.active != this.state.active;
+  }
+
+  onMouseOver(idx) {
+    this.setState({'hover': idx});
+  }
+
+  onMouseOut(e) {
+    this.setState({'hover': null});
+  }
+
+  compareStyles(component, idx) {
+    return idx == this.state.active ? component.activeStyles : idx == this.state.hover ? component.hoverStyles : component.nonActiveStyles
+  }
+
+  render() {
+    let navCount = 5;
+    // let navArr = [];
+    let navArr = ["About Me", "Web", "Design", "Video", "Contact"];
+    // for (let i = 0; i < navCount; i++) {
+    //   navArr.push(i);
+    // }
+
+    return (
+      <Fullpage active={this.updateActiveState}>
+
+        <TopNav {...topNavOptions}>
+          {navArr.map((n, idx) => {
+            return <span key={idx} ref={idx} className="topnav-items">{navArr[idx]}</span>
+          }, this)}
+        </TopNav>
+
+        <Slide style={{backgroundColor: '#FF574F'}}>
+        </Slide>
+        <Slide style={{backgroundColor: '#D7A069'}}></Slide>
+        <Slide style={{backgroundColor: '#9F9B9F'}}></Slide>
+        <Slide style={{backgroundColor: '#FFBBB8'}}></Slide>
+        <Slide style={{backgroundColor: '#2B2C28'}}></Slide>
+
+        <SideNav {...sideNavOptions}>
+          {navArr.map((n, idx) => {
+            return <div key={idx} ref={idx}>&#x25CF;</div>
+          }, this)}
+        </SideNav>
+
+      </Fullpage>
+    );
+  }
+};
+
+module.exports = FullpageReact;
