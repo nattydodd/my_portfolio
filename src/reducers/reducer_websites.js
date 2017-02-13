fc1ef776083047d20524447b55094dbe92f59913
Natalie Dodd <nataliedodd89@gmail.com>
2017-02-13 07:54:08 +0100

    changed server to run on port 5000

--- src/reducers/reducer_websites.js
+++ src/reducers/reducer_websites.js
@@ -3,27 +3,27 @@ export default function() {
       { title: 'PackApp',
         url: "https://dl.dropboxusercontent.com/u/80500150/comp1.png",
         description:"While studying at Bitmaker, we had the challenge of breaking into groups of 4, and building something with a 2 week deadline. My group chose to create a fun app which helps you to visualize your wardrobe and pack for a holiday. It includes an outfit checker which allows you to mix and match clothing items, as well as a drag and drop feature created with JQuery UI where the user can drag items and save them into their suitcase. We also integrated a weather API to show the weather of your destination, to add to the virtual packing experience.",
-        link: "https://pack-app.herokuapp.com"
+        link: "http://pack-app.herokuapp.com"
       },
       { title: 'Share Your Canada',
         url:"https://dl.dropboxusercontent.com/u/80500150/comp2.png",
         description:"Show Us Your Canada is an online contest and marketing strategy to promote tourism in the Muskoka Region of Canada. It target expats living abroad, and allows them to upload photos and stories depicting their favourite Muskoka memories. I created this app using Rails and JQuery to allow the users to add Instagram-like filters to their submissions. Users can vote, and each vote is verified using an email verification system as well validated by unique IP address. Submissions are also shareable via Instagram and Twitter.",
-        link: "https://show-us-your-canada.herokuapp.com"
+        link: "http://show-us-your-canada.herokuapp.com"
       },
       { title: 'Financial Advice for All',
         url:"https://dl.dropboxusercontent.com/u/80500150/comp3.png",
         description:"Working with Enterprise Canada, I was often asked to build custom Nationbuilder or WordPress Templates, and add complex features to them using Javascript or external APIs. Financial Advice for All, a coalition website to protect access to financial advice for Canadians, is one example. I built this website using Nationbuilder, and then integrated the Open North API to allow users to contact the correct local MPP according to their postal code.",
-        link: "https://www.financialadviceforall.com"
+        link: "http://www.financialadviceforall.com"
       },
       { title: 'Road Closure Generator',
         url: "https://dl.dropboxusercontent.com/u/80500150/comp4.png",
         description:"I created a Road Closure Generator using Rails, JQuery and the Google Maps API for a client called BlackBird, which deals with road closures in the Durham Region of Ontario. Prior to the created of this app, the company had to manually photoshop lines on a map to show the closures. However, now they are able to click on a map and save the coordinates, which generates a line automatically.",
-        link: "https://road-closure-generator.herokuapp.com"
+        link: "http://road-closure-generator.herokuapp.com"
       },
       { title: 'The Human Network',
       url: "https://dl.dropboxusercontent.com/u/80500150/comp5.png",
       description:"I took on a front-end freelance project for a Start-Up company directly upon graduating Bitmaker to improve my speed and skill with CSS and Javascript. Working with one other team member, we created a mock website for a political social networking website. We met with the client, assisted with creative direction and built a product that could be used to pitch the idea to investors for further funding.",
-      link: "https://www.thehumannetwork.ca"
+      link: "http://www.thehumannetwork.ca"
       }
   ]
 }
